a72f9bdcdecdb5e7b668a2e76ce2bb55d17237a2
ShadyShawkat <shadyshawkat91@gmail.com>
2022-03-14 22:47:56 +0200

    Finish the implementation of the main method

--- app.rb
+++ app.rb
@@ -1,15 +1,5 @@
 class App
-  def initialize
-    puts "
-    1) List all books
-    2) List all music albums
-    3) List of games
-    4) List all genres (e.g 'Comedy', 'Thriller')
-    5) List all labels (e.g. 'Gift', 'New')
-    6) List all authors (e.g. 'Stephen King')
-    7) Add a book
-    8) Add a music album
-    9) Add a game
-    "
+  def options(answer)
+    answer
   end
 end
