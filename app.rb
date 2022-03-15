19427744fb4552278987eb144d4f3a6520cd26b1
Santiago Velosa <santiagovelosa@gmail.com>
2022-03-15 17:20:29 -0500

    Add list_genre.rb

--- app.rb
+++ app.rb
@@ -1,19 +1,23 @@
 require_relative './Classes/list_musicalbums'
 require_relative './Classes/add_musicalbum'
+require_relative './Classes/list_genre'
 
 class App
-  attr_reader :items
+  attr_reader :items, :labels, :genres, :authors
 
   def initialize
     @items = []
+    @authors = []
+    @genres = []
+    @labels = []
   end
 
   def options(answer) # rubocop:disable Metrics/CyclomaticComplexity/
     case answer
-    when '0' then ListBooks.new.list(@items)
+    when '0' then 'List of books'
     when '1' then ListMusicalbums.new.list_musicalbums(@items)
     when '2' then 'List of games'
-    when '3' then "List all genres (e.g 'Comedy', 'Thriller')"
+    when '3' then ListGenre.new.list_genres(@genres)
     when '4' then 'List of labels'
     when '5' then "List all authors (e.g. 'Stephen King')"
     when '6' then 'Add a book'
