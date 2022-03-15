6ffe74a418d66026b5e87c544e822a0161bd3e99
ShadyShawkat <shadyshawkat91@gmail.com>
2022-03-15 17:33:33 +0200

    Add game class with all methods

---
+++ Classes/game.rb
@@ -0,0 +1,16 @@
+require_relative '../item'
+require 'date'
+
+class Game < Item
+  attr_accessor :publish_date, :multiplayer, :last_played_at
+
+  def initialize(*args, multiplayer, last_played_at)
+    super(*args)
+    @multiplayer = multiplayer
+    @last_played_at = last_played_at
+  end
+
+  def can_be_archived?
+    super && (DateTime.now.year - Date.parse(last_played_at).year > 2)
+  end
+end
