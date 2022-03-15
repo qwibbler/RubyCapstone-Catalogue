738c9a6c4d5cb2b3a9874f4ea1dca9370af2607e
Santiago Velosa <santiagovelosa@gmail.com>
2022-03-15 10:26:59 -0500

    Finish musiclabum.rb

--- musicalbum.rb
+++ musicalbum.rb
@@ -1,5 +1,14 @@
-class Musiclabum
-  def initialize(on_spotify)
+require './item'
+
+class Musicalbum < Item
+  def initialize(*args, on_spotify: false)
+    super(*args)
     @on_spotify = on_spotify
+  end
+
+  def can_be_archived?
+    return true if super && @on_spotify
+
+    false
   end
 end
