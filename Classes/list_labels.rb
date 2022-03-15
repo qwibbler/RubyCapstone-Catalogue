98cb1698d53df3fedec05ae2522f67c2b4f25f1b
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-03-15 16:17:18 -0600

    Linters

--- Classes/list_labels.rb
+++ Classes/list_labels.rb
@@ -2,6 +2,6 @@ class ListLabels < ListData
   def list_labels(data)
     return puts "No Labels found\n\n" if data.empty?
 
-    data.each_with_index { |label, index| list(index, {'Title' => label.title, 'Color' => label.color}) }
+    data.each_with_index { |label, index| list(index, { 'Title' => label.title, 'Color' => label.color }) }
   end
 end
