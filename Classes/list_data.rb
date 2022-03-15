3257078cd675f9b204e6093f8d74c3147dc5fdbe
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-03-15 12:06:14 -0600

    Change commas for tabs

--- Classes/list_data.rb
+++ Classes/list_data.rb
@@ -1,14 +1,14 @@
 class ListData
   def list(index, data)
     print "#{index + 1}) "
-    data.each { |key, value| print "#{key}: #{value}, " }
+    data.each { |key, value| print "#{key}: #{value} \t" }
     puts
   end
 end
 
 class ListBooks < ListData
   def list(data)
-    data.each_with_index do |book, index|
+    data.select { |item| item.instance_of?(Book) }.each_with_index do |book, index|
       super(index, { 'ID' => book.id, 'Publisher' => book.publisher, 'Publish Date' => book.publish_date })
     end
     nil
