f9b56b18ebeb26049caab151a51d79bc14ecb806
Santiago Velosa <santiagovelosa@gmail.com>
2022-03-15 11:26:25 -0500

    Update genre.rb

--- Classes/genre.rb
+++ Classes/genre.rb
@@ -10,7 +10,7 @@ class Genre
   end
 
   def add_item(item)
-    @items.push(item) if item.instance_of?(Item) && !@items.include?(item)
+    @items << item if item.instance_of?(Item) && !@items.include?(item)
     item.add_genre(self)
   end
 end
