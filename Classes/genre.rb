28e18530a57f234e6df34759156d0f3bd0a07823
Santiago Velosa <santiagovelosa@gmail.com>
2022-03-15 11:16:40 -0500

    Finish genre.rb

--- Classes/genre.rb
+++ Classes/genre.rb
@@ -2,9 +2,16 @@ require './item'
 
 class Genre
   attr_reader :id, :items
-  def initialize(name)
+
+  def initialize(name, *args)
+    super(*args)
     @id = Random.rand(1..1_000_000)
     @name = name
     @items = []
+  end
+
+  def add_item(item)
+    @items.push(item) if item.instance_of?(Item) && !@items.include?(item)
+    item.add_genre(self)
   end
 end
