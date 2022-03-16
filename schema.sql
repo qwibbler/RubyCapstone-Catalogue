168f9a06810f147954d1393b1e3aa3af97a1692e
ShadyShawkat <shadyshawkat91@gmail.com>
2022-03-16 22:37:22 +0200

    Add tables for games and authors

---
+++ schema.sql
@@ -0,0 +1,24 @@
+CREATE TABLE items (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  publish_date DATE NOT NULL,
+  archived BOOLEAN DEFAULT FALSE
+)
+
+CREATE TABLE games (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  item_id INT REFERENCES items (id),
+  multiplayer BOOLEAN NOT NULL,
+  last_played_at DATE NOT NULL
+)
+
+CREATE TABLE authors (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL
+)
+
+CREATE TABLE items_authors (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  item_id INT REFERENCES items (id),
+  author_id INT REFERENCES authors (id)
+)
