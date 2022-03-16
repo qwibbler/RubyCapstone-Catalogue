39b741717c3e695c1eab358c4e7e27c57c0802f3
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-03-16 14:42:26 -0600

    Add Books and Labels schema

--- schema.sql
+++ schema.sql
@@ -22,3 +22,22 @@ CREATE TABLE items_authors (
   item_id INT REFERENCES items (id),
   author_id INT REFERENCES authors (id)
 )
+
+CREATE TABLE books (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  item_id INT REFERENCES items (id),
+  publisher VARCHAR(50) NOT NULL,
+  cover_state VARCHAR(50) NOT NULL
+)
+
+CREATE TABLE labels (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  title VARCHAR(50) NOT NULL,
+  color VARCHAR(50) NOT NULL
+)
+
+CREATE TABLE items_labels (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  item_id INT REFERENCES items (id),
+  label_id INT REFERENCES labels (id)
+)
